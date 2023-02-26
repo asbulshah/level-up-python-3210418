6f8b01ba8f743b3fe134805f2c472e4155a76e27
Asbul Shah <83455525+asbulshah@users.noreply.github.com>
2023-02-26 13:02:33 +0000

    okkok

--- src/02 Identify a Palindrome/palindrome_1.py
+++ src/02 Identify a Palindrome/palindrome_1.py
@@ -1,11 +1,12 @@
 import re
 
+
 def is_palindrome(phrase):
-    forwards = ''.join(re.findall(r'[a-z]+',phrase.lower()))
+    forwards = ''.join(re.findall(r'[a-z]+', phrase.lower()))
     backwards = forwards[::-1]
     return forwards == backwards
+
 
 if __name__ == '__main__':
     phrase_input = input("Enter a phrase\n")
     print(is_palindrome(phrase_input))
-    
